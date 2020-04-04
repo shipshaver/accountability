83eff63d63046fbe1d0d9d4ddf7dee81a342c5f3
Lan Le <lanle@hotmail.nl>
2020-04-04 06:27:46 +0200

    bar color depending on progress/step

--- public/scripts/progressBars.js
+++ public/scripts/progressBars.js
@@ -2,19 +2,24 @@ console.log("Progress bar script loading..")
 displayDate();
 setColor();
 
-// show current date
+// announce current date
 function displayDate() {
     // get today
     const d = new Date();
     const n = d.getDay();
     let dateDisplay = document.getElementById("date");
-    dateDisplay.appendChild(document.createTextNode("Day" + n + "of the week"));
+    //dateDisplay.appendChild(document.createTextNode("Today it is day " + n + " of the week"));
+    dateDisplay.appendChild(document.createTextNode("Today it is " + d));
 }
 
 // start bar with color depending on progress
 function setColor() {
     let step = 0;
-    let progress = document.getElementById("progress");
+    let bars = document.getElementsByClassName("progress");
+    for (let i = 0; i < bars.length; i++) {
     // delay color animation to start midway
-    progress.setAttribute("animation-delay", -step);
+    //progress.setAttribute("animation-delay", -step + "s");
+        console.log(bars[i]);
+        bars[i].style.backgroundColor = "hsl("+step+",100%,50%)";
+    }
 }
