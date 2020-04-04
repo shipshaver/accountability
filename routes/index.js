3946037ad6b703603bc54ab49f8fec9bdd1b0bf2
Lan Le <lanle@hotmail.nl>
2020-04-04 18:08:55 +0200

    dayduration

--- routes/index.js
+++ routes/index.js
@@ -3,37 +3,18 @@ var router = express.Router();
 
 const sqlite3 = require("sqlite3").verbose();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  console.log(req.body);
-
-  let db = new sqlite3.Database('./data.db');
-
-  let sql = `SELECT * FROM streaks`;
-
-  db.serialize(function () {
-    db.all(sql, [], (err, rows) => {
-      if (err != null) {
-        return next(err);
-      }
-      res.render('index', { title: 'Daily Accountability', streak_list: rows });
-    });
-  });
-
-  db.close();
-});
-
 //let lastID = 0;
 
+// add new streak using startdate and update last streak
 router.get('/:date', function (req,res,next) {
   let db = new sqlite3.Database('./data.db');
-  
+
   let date = req.params.date;
 
   // set last row id (last day)
   let sql = `UPDATE streaks
               SET endDate = ?
-              WHERE id = (SELECT MAX(ID) FROM TABLE)`;
+              WHERE id = (SELECT MAX(ID) FROM streaks)`;
 
   // set today as end date of previous streak
   db.run(sql, [date], function(err) {
@@ -58,5 +39,27 @@ router.get('/:date', function (req,res,next) {
 
   db.close();
 });
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  console.log(req.body);
+
+  let db = new sqlite3.Database('./data.db');
+
+  let sql = `SELECT * FROM streaks`;
+
+  db.serialize(function () {
+    db.all(sql, [], (err, rows) => {
+      if (err != null) {
+        return next(err);
+      }
+      res.render('index', { title: 'Daily Accountability', streak_list: rows });
+    });
+  });
+
+  db.close();
+});
+
+
 
 module.exports = router;
